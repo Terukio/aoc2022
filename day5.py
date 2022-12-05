387c2e91ee2f1d1017bddbd65176496d1a6173e5
Terukio <94190269+Terukio@users.noreply.github.com>
2022-12-05 11:17:32 -0500

    Final(?) updated including regex.

--- day5.py
+++ day5.py
@@ -26,8 +26,10 @@ def main():
     response = int(input('1 for example, 2 for actual: '))
     if response == 1:
         file = r'day5ex.txt'
+        number_of_stacks = 3
     else:
         file = r'day5data.txt'
+        number_of_stacks = 9
 
     with open(file) as f:
         data = f.read()
@@ -37,33 +39,16 @@ def main():
     instructionRegex = re.compile(r'move (\d+) from (\d+) to (\d+)')
     instructions = instructionRegex.findall(instructions)
 
-    structure = structure.split('\n')
-    
-    newStructure = []
-    for j in range(len(structure)):
-        items = []
-        for i in range(len(structure[j])):
-            if (i + 1) % 4 == 0:
-                continue
-            elif structure[j][i] == '[':
-                continue
-            elif structure[j][i] == ']':
-                continue
-            elif structure[j][i] != ' ':
-                items.append(structure[j][i])
-            elif (i + 1) % 2 == 0:
-                items.append(' ')
-        newStructure.append(items)
+    structureRegex = re.compile(r'(\s{3}|\[\w\])\s')
+    structure = structureRegex.findall(structure)
 
-    stacks = [[] for _ in range(len(newStructure[-1]))]
+    stacks = []
 
-    for info in newStructure[::-1]:
-        for i in range(len(info)):
-            if info[i] != ' ':
-                stacks[i].append(info[i])
-
-    for i in range(len(stacks)):
-        del stacks[i][0]
+    for i in range(number_of_stacks):
+        stacks.append([structure[i + (j * 3)] for j in range(number_of_stacks)][::-1])
+        while stacks[i][-1] == '   ':
+            del stacks[i][-1]
+    print(stacks)
 
     stacks2 = copy.deepcopy(stacks)
 
